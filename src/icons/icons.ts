263113ed62f34815b922585a5e624e423c0616ff
KingaPrivate <kin.stanek@gmail.com>
2021-03-13 20:23:21 +0100

    Add icons

---
+++ src/icons/icons.ts
@@ -0,0 +1,8 @@
+import { ReactComponent as arrowDown } from './arrow-down.svg';
+import { ReactComponent as arrowUp } from './arrow-up.svg';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  arrowDown,
+  arrowUp
+};
