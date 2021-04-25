b05cc6b71cd6f44f5e00c8f6a4f0ad51d7514b5c
KingaPrivate <kin.stanek@gmail.com>
2021-04-25 20:27:06 +0200

    Add icons

--- src/icons/icons.ts
+++ src/icons/icons.ts
@@ -1,8 +1,24 @@
 import { ReactComponent as arrowDown } from './arrow-down.svg';
 import { ReactComponent as arrowUp } from './arrow-up.svg';
+import { ReactComponent as car } from './car.svg';
+import { ReactComponent as paint } from './paint.svg';
+import { ReactComponent as people } from './people.svg';
+import { ReactComponent as crashedCar } from './crashedCar.svg';
+import { ReactComponent as location } from './location.svg';
+import { ReactComponent as phone } from './phone.svg';
+import { ReactComponent as email } from './email.svg';
+import { ReactComponent as facebook } from './facebook.svg';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   arrowDown,
-  arrowUp
+  arrowUp,
+  car,
+  paint,
+  people,
+  crashedCar,
+  location,
+  phone,
+  email,
+  facebook
 };
