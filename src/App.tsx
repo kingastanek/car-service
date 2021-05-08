f928872b082cf57c294fd3a965b3af93ac7fd22d
KingaPrivate <kin.stanek@gmail.com>
2021-05-08 09:57:52 +0200

    iframe v1

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,19 @@ import { MainPage, AboutMePage, TinsmithingPage, VarnishingPage } from 'views';
 function App() {
   return (
     <>
-      <NavBar />
-      <MainPage />
-      <AboutMePage />
-      <TinsmithingPage />
-      <VarnishingPage />
+      <iframe
+        src='http://master.dszdl6dg07r.amplifyapp.com/'
+        style={{
+          width: '100vw',
+          height: '100vh',
+        }}
+      >
+        <NavBar />
+        <MainPage />
+        <AboutMePage />
+        <TinsmithingPage />
+        <VarnishingPage />
+      </iframe>
     </>
   );
 }
